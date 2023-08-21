3747866c95cafc7b3ed5ba74bf1803a83a946d27
Victor Chin <victor@objectmage.com>
2023-08-21 13:41:04 -0500

    Numeric Type and String, stack and heap, value vs reference

--- victor/victor-class-solution/HelloWorld/Program.cs
+++ victor/victor-class-solution/HelloWorld/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//int age = 50;
+Console.WriteLine($"int: {int.MaxValue},{int.MinValue}");
+Console.WriteLine($"ushort: {ushort.MaxValue},{ushort.MinValue}");
+
+Console.WriteLine($"float: {float.MaxValue},{float.MinValue}");
+Console.WriteLine($"double: {double.MaxValue},{double.MinValue}");
+
+Console.WriteLine($"decimal: {decimal.MaxValue},{decimal.MinValue}");
+
+string s1 = "Hello \"World\"";
+string s2 = "Hello \"World\"";
+Demo d1 = new Demo() { x=10 };
+Demo d2 = new Demo() { x=10 };
+
+d1.A();
+Console.WriteLine(s1==s2);
+Console.WriteLine(d1==d2);
+
+class Demo {
+    public int x { get; set; }
+    internal void A() {
+        int i = 0;
+        Console.WriteLine("This is A before calling B");
+        B();
+        Console.WriteLine("This is A after calling B");
+    }
+
+    internal void B()
+    {
+        int j = 0;
+        Console.WriteLine("This is B before calling C");
+        C();
+        Console.WriteLine("This is B after calling C");
+    }
+    internal void C()
+    {
+        int k = 0;
+        Console.WriteLine("This is C");
+    }
+}
