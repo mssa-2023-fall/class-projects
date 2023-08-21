ff1d992766ce1656c7b0087f850724fd2f4bbb82
BbyWiz <96n_lawrence@outlook.com>
2023-08-21 14:17:10 -0400

    Commit 2

---
+++ Larry/larry-project1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, World!");
+//int age = 50;
+Console.WriteLine($"{int.MaxValue},{int.MinValue}");
+Console.WriteLine($"{ushort.MaxValue}, {ushort.MinValue}");
+
+float f1 = float.MaxValue;
+double d1 = double.MaxValue;
+
+decimal dec1 = decimal.MaxValue;
+Console.ReadLine();
