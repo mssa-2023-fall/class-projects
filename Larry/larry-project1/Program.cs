0c38d51947452cee33ed4592890b44daf2ad7f67
BbyWiz <96n_lawrence@outlook.com>
2023-08-21 14:41:50 -0400

    Numeric Type and String with value types and reference types..

--- Larry/larry-project1/Program.cs
+++ Larry/larry-project1/Program.cs
@@ -3,8 +3,46 @@
 Console.WriteLine($"{int.MaxValue},{int.MinValue}");
 Console.WriteLine($"{ushort.MaxValue}, {ushort.MinValue}");
 
-float f1 = float.MaxValue;
-double d1 = double.MaxValue;
+Console.WriteLine($" float: {float.MinValue} , {float.MaxValue}");
+Console.WriteLine($" double: {double.MaxValue}, {double.MinValue}");
 
-decimal dec1 = decimal.MaxValue;
+Console.WriteLine($" decimal: {decimal.MaxValue}, {decimal.MinValue}");
 Console.ReadLine();
+
+//Strings
+string s = "Hello \"World\"";
+string s2 = "Hello \"World\"";
+//RETURNS TRUE
+Console.WriteLine(s ==  s2);
+//RETURNS FALSE
+
+Demo d1 = new Demo() { x = 10 };
+Demo d2 = new Demo() { x = 10 };
+
+d1.A();
+Console.ReadLine();
+class Demo
+{
+    public int x { get; set; }
+    
+    internal void A()
+    {
+        int i = 0;
+        Console.WriteLine("This is A before calling B");
+        B();
+        Console.WriteLine("This is A after calling B");
+    }
+
+    internal void B()
+    {
+        int j = 0;
+        Console.WriteLine("This is B before calling C");
+        C();
+        Console.WriteLine("This is B after calling C");
+    }
+    internal void C()
+    {
+        int k = 0;
+        Console.WriteLine("This is C");
+    }
+}
