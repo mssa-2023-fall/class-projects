7670c704a441763396418d30e27928f99674814a
Leo <leonard.a.karoly@gmail.com>
2023-08-23 10:38:29 -0700

    Created a NuGet Texas Hold' em Game

---
+++ Leo/PackageExercise/LearnPackageManager/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TexasHoldem.Logic.Cards;
+using TexasHoldem.Logic.Helpers;
+
+string flush = firstTry();
+int tries = 1;
+while (flush != "StraightFlush")
+{
+    flush = firstTry();
+    tries++;
+}
+Console.WriteLine($"Only took {tries} tries!");
+static string firstTry()
+{
+    Deck aDeck = new Deck(); //CTR + . allows for intell-essence to offer solution
+    var firstCard = aDeck.GetNextCard();
+    var secondCard = aDeck.GetNextCard();
+    Console.WriteLine($"Your cards are {firstCard} and {secondCard}");
+
+    List<Card> cards = new List<Card>();
+    for (int i = 0; i < 5; i++) cards.Add(aDeck.GetNextCard());
+
+    Console.Write("The River is: ");
+    foreach (Card card in cards) Console.Write(card + ", ");
+
+    cards.Add(firstCard);
+    cards.Add(secondCard);
+    var result = Helpers.GetHandRank(cards);
+    Console.WriteLine(result.ToString());
+    return result.ToString();
+}
