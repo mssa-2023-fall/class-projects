04b1855743f88d9a79ba3d48aeb54bfd6ec76ae1
Geoff-San <geoffrey.sanford@outlook.com>
2023-08-28 14:23:07 -0500

    Learning Async with hashing files in directory

---
+++ SyncFileHashes/AsyncFileHasher/Program.cs
@@ -0,0 +1,19 @@
+﻿using SmartFiles;
+using System.Diagnostics;
+
+var AllFiles = Directory.GetFiles("C:\\Source\\Repos\\class-projects\\SampleText\\");
+
+List<Task> listofComputeHashTask = new List<Task>();
+Stopwatch sw = Stopwatch.StartNew();
+
+for(int i = 0; i < 100; i++)
+{
+	foreach(var item in AllFiles)
+	{
+		var _temp = new SmartFile(item);
+		listofComputeHashTask.Add(_temp.GetHashStringAsync());
+	}
+}
+await Task.WhenAll(listofComputeHashTask);
+sw.Stop();
+Console.WriteLine($"Elapsed Time with Sync calls:{sw.ElapsedMilliseconds} ms");
