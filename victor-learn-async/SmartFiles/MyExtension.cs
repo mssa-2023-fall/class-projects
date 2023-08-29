c35f8454b27ff14f1ed3e86e69ee2ae896f57a10
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-08-29 10:47:54 -0500

    Added new static class MyExtension.cs in the library project "SmartFiles" Within the static class we created an extension method to add ProcessList method to "ANY" List<T>, we will implement Filter Method as extension method next.

---
+++ victor-learn-async/SmartFiles/MyExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFiles
+{
+    public static class MyExtension //extension methods must exist in static class
+    {
+        //the first parameter indicates the Type which you would like to extend
+        public static void ProcessList<T>(this List<T> inputList, Action<T> toPerform) { 
+            foreach (var item in inputList)
+            {
+                toPerform(item);
+            }
+        }
+        
+    }
+}
