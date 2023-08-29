8efbd1ed3e0cfd4f957ec5f71caa506d32a6b011
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-08-29 10:59:05 -0500

    Completed both Extension methods in MyExtension and updated Program.cs to use one liner.

--- victor-learn-async/DelegateAndExtensionMethods/Program.cs
+++ victor-learn-async/DelegateAndExtensionMethods/Program.cs
@@ -17,27 +17,29 @@ internal class Program
         {
             allSmartFiles.Add(new SmartFile(fileName));
         }
-                
-        List<SmartFile> bigFiles = Filter<SmartFile>(allSmartFiles,(sf)=> sf.File.Length>300*1024);
+
+        allSmartFiles
+            .Filter((sf)=> sf.File.Length>300*1024)
+            .ProcessList((aFile) => Console.WriteLine($"{aFile.File.FullName} has {aFile.File.Length} bytes of data"));
         //ProcessList(bigFiles, (aFile) => Console.WriteLine($"{aFile.File.FullName} has {aFile.File.Length} bytes of data"));
-        bigFiles.ProcessList((aFile) => Console.WriteLine($"{aFile.File.FullName} has {aFile.File.Length} bytes of data"));
+
         // static void ProcessList(List<SmartFile> inputList, Action<SmartFile> actionToPerform)
         // we will be using this method to replace the repetitious foreach loop below
 
-        List<SmartFile> filesWithNumericStart = Filter<SmartFile>(allSmartFiles,
+        allSmartFiles.Filter(
             (sf)=> {
                 string initialLetter = sf.File.Name.Substring(0, 1);
                 return int.TryParse(initialLetter, out int x);
                 }
-            );
+            )
+            .ProcessList((aFile) => Console.WriteLine($"{aFile.File.FullName} has initial letter that is a number"));
         //ProcessList(filesWithNumericStart, (aFile) => Console.WriteLine($"{aFile.File.FullName} has initial letter that is a number"));
-        filesWithNumericStart.ProcessList((aFile) => Console.WriteLine($"{aFile.File.FullName} has initial letter that is a number"));
-        
-        StreamReader sr = filesWithNumericStart[0].File.OpenText();
+
+
+        StreamReader sr = allSmartFiles[0].File.OpenText();
         List<string> lines = new List<string>(sr.ReadToEnd().Split(new char[] { '\n' }));
-                
-        var resultLines = Filter<string>(lines, l => l.Length > 20);
-        resultLines.ProcessList((aLine) => Console.WriteLine(aLine));
+
+        lines.Filter<string>(l => l.Length > 20).ProcessList((aLine) => Console.WriteLine(aLine));
     }
 
     private static void ProcessList<T>(List<T> inputList, Action<T> actionToPerform)
--- victor-learn-async/SmartFiles/MyExtension.cs
+++ victor-learn-async/SmartFiles/MyExtension.cs
@@ -10,12 +10,25 @@ namespace SmartFiles
     public static class MyExtension //extension methods must exist in static class
     {
         //the first parameter indicates the Type which you would like to extend
-        public static void ProcessList<T>(this List<T> inputList, Action<T> toPerform) { 
+        public static void ProcessList<T>(this List<T> inputList, Action<T> toPerform) {
             foreach (var item in inputList)
             {
                 toPerform(item);
             }
         }
-        
+
+        public static List<T> Filter<T>(this List<T> input, Func<T, bool> predicate) {
+
+            List<T> result = new();
+            foreach (var item in input)
+            {
+                if (predicate.Invoke(item)) {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
     }
+        
 }
+
