156b73392fdbae713d8f8ca453295326cf42a42e
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-08-29 09:44:59 -0500

    replace foreach statement with Generic Method ProcessList so the code is more reusable and cleaner

--- victor-learn-async/DelegateAndExtensionMethods/Program.cs
+++ victor-learn-async/DelegateAndExtensionMethods/Program.cs
@@ -15,32 +15,24 @@ internal class Program
         {
             allSmartFiles.Add(new SmartFile(fileName));
         }
-
+                
         List<SmartFile> bigFiles = Filter<SmartFile>(allSmartFiles,(sf)=> sf.File.Length>300*1024);
-        
-        ProcessList(bigFiles, (file) => Console.WriteLine(file.Name));
+        ProcessList(bigFiles, (aFile) => Console.WriteLine($"{aFile.File.FullName} has {aFile.File.Length} bytes of data"));
         // static void ProcessList(List<SmartFile> inputList, Action<SmartFile> actionToPerform)
         // we will be using this method to replace the repetitious foreach loop below
-        foreach (var aFile in bigFiles)
-        {
-            Console.WriteLine($"{aFile.File.FullName} has {aFile.File.Length} bytes of data");
-        }
-
+        
         List<SmartFile> filesWithNumericStart = Filter<SmartFile>(allSmartFiles,
             (sf)=> {
                 string initialLetter = sf.File.Name.Substring(0, 1);
                 return int.TryParse(initialLetter, out int x);
                 }
             );
-        foreach (var aFile in filesWithNumericStart)
-        {
-            Console.WriteLine($"{aFile.File.FullName} has initial letter that is a number");
-        }
+        ProcessList(bigFiles, (aFile) => Console.WriteLine($"{aFile.File.FullName} has initial letter that is a number"));
 
         StreamReader sr = filesWithNumericStart[0].File.OpenText();
         List<string> lines = new List<string>(sr.ReadToEnd().Split(new char[] { '\n' }));
         var resultLines = Filter<string>(lines, l => l.Length > 20);
-        foreach (var line in resultLines) { Console.WriteLine(line); }
+        ProcessList(resultLines, (aLine) => Console.WriteLine());
     }
 
     private static void ProcessList<T>(List<T> inputList, Action<T> actionToPerform)
