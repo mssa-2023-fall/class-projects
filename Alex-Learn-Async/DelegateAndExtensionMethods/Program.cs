3188caa52c34c32534beda20f39ffe6deac4f5b6
AlejandroGonzalezDe <alejandro.gonzalezde@outlook.com>
2023-08-29 11:10:11 -0400

    Print LIst Generic Method and Code Correction

--- Alex-Learn-Async/DelegateAndExtensionMethods/Program.cs
+++ Alex-Learn-Async/DelegateAndExtensionMethods/Program.cs
@@ -16,38 +16,57 @@ internal class Program
         {
             allSmartFiles.Add(new SmartFile(fileName));
         }
-        List<SmartFile> bigFiles = Filter(allSmartFiles,(sf)=>sf.File.Length>300*1024);
-        foreach (var bigFile in bigFiles)
-        {
-            Console.WriteLine($"{bigFile.File.FullName} has {bigFile.File.Length} bytes of data");
+
+        List<SmartFile> bigFiles = Filter<SmartFile>(allSmartFiles,(sf)=>sf.File.Length>300*1024);
+        PrintList(bigFiles, (afile) => Console.WriteLine($"{afile.File.FullName} has {afile.File.Length} bytes of data"));
+
+            // static void PrintList(List<T> inputList, Action<T>
+
+            List<SmartFile> filesWithNumbersStart = Filter<SmartFile>(allSmartFiles,
+                (sf) =>
+                {
+                    string initialLetter = sf.File.Name.Substring(0, 1);
+                    return int.TryParse(initialLetter, out int x);
+                }
+                );
+            PrintList(bigFiles, (afile) => Console.WriteLine($"{afile.File.FullName} has initial letter that is a number."));
+
+            StreamReader sr = filesWithNumbersStart[0].File.OpenText();
+            List<string> lines = new List<string>(sr.ReadToEnd().Split(new char[] { '\n' }));
+            var resultLines = Filter<string>(lines, l => l.Length > 20);
+            PrintList(resultLines, (afile) => Console.WriteLine());
+
         }
-        List<SmartFile> filesWithNumbersStart = Filter(allSmartFiles,
-            (sf) => { string initialLetter = sf.File.Name.Substring(0, 1);
-                return int.TryParse(initialLetter, out int x);
-            }
-            );
-        foreach (var bigFile in bigFiles)
-        {
-            Console.WriteLine($"{bigFile.File.FullName} has initial letter that is a number");
-        }
+
+    private static void PrintList<T>(List<T> inputList, Action<T> actionToPerform)
+    {
+        foreach (var file in inputList)
+        { actionToPerform.Invoke(file); }
     }
-   /*  static List<SmartFile> FilterFileSize(List<SmartFile> inputList)
 
-        List<SmartFile> result = new List<SmartFile>();
-      foreach (var file in inputList)
-      {
-          if (file.File.Length>200*1024) { result.Add(file); }  
-       }
-      return result;
-    
-     }*/
-
-    <summary>
-        this makes multiple version of Filter method obsolete
-        we use Fun
+    static List<T> Filter<T>(List<T> inputList, Func<T, bool> predicate)
+    {
+        List<T> result = new List<T>();
+        foreach (var file in inputList)
+        {
+            if (predicate.Invoke(file)) { result.Add(file); }
+        }
+        return result;
+    }
 
 
-    /*static List<SmartFile> FilterFiles(List<SmartFile> inputList, Func<SmartFile,bool> predicate)
+    /*static List<SmartFile> FilterFileSize(List<SmartFile> inputList)
+
+         List<SmartFile> result = new List<SmartFile>();
+       foreach (var file in inputList)
+       {
+           if (file.File.Length>200*1024) { result.Add(file); }  
+        }
+       return result;
+
+      }*/
+
+    static List<SmartFile> FilterFiles(List<SmartFile> inputList, Func<SmartFile, bool> predicate)
     {
         List<SmartFile> result = new List<SmartFile>();
         foreach (var file in inputList)
@@ -55,25 +74,23 @@ internal class Program
             if (predicate.Invoke(file)) { result.Add(file); }
         }
         return result;
-    }*/
+    }
     //Step 1: Add<T> to the end of method name to indicate this is a generic method
     //Step 2: replace all references to List<SmartFile> to List<T>
-    static List<T> Filter<T>(List<T> inputList, Func<T, bool> predicate)
-    {
-        List<T> result = new List<T>();
-        foreach (var file in inputList)
-        {
-            if(predicate.Invoke(file)) { result.Add(file); }
-        }
-        return result;
-    }
-  /*     static List<SmartFile> FilterFilesByInitialLetter(List<SmartFile> inputList)
-       {
-            List<SmartFile> result = new List<SmartFile>();
-            foreach (var file in inputList)
-            {
-                if (int.TryParse(file.File.Name.Substring(0, 1), out int x)) { result.Add(file); }
-            }
-            return result;
-        }*/
+
+    /*     static List<SmartFile> FilterFilesByInitialLetter(List<SmartFile> inputList)
+         {
+              List<SmartFile> result = new List<SmartFile>();
+              foreach (var file in inputList)
+              {
+                  if (int.TryParse(file.File.Name.Substring(0, 1), out int x)) { result.Add(file); }
+              }
+              return result;
+          }*/
+    /*void methodName(params, params ParamArrayAttribute,)
+        returntype methodName(param1, param2)
+        // accept a SmartFIle and return a boolean
+        bool SomeMethod(SmartFile file) { //logic}
+        (SmartFile aFile)=> aFile.File.Length>100*/
+
 }
